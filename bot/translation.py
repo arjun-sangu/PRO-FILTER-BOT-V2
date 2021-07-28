51d52d35fcfe515eeb2b2727768147c35d3784b5
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-07-28 19:27:54 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,13 +6,7 @@ class Translation(object):
     
     START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
 
-<b>I am a Pro AutoFilterBot V2....😜</b>
-
-<b>Make me an admin for your group and channel then connect me....🎉</b>
-
-<b>🙂Watch the video below to build an Pro Autofilter bot V2 like me...</b>
-
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
+<b>I AM GROUP HELPER BOT FOR @NEW_MLM_HD_MOVES....😜</b>
 
 <b>Press /help to know about available commands🤪</b>"""    
     
@@ -46,18 +40,14 @@ class Translation(object):
             
             -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
             
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b><a href="https://t.me/NEW_MLM_HD_MOVES">© MOVIE REQESTING GROUP</a></b>
 """
     
-    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
+    ABOUT_TEXT = """<b>➥ 👨‍✈️Name</b> : <b>@NEW_MLM_HD_MOVES HELPER RObot</b>
 
-<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/AlbertEinstein_TG">👤Albert Einstein</a></b>
+<b>>➥ ⭕MOVIE REQESTING GROUP</b> : @NEW_MLM_HD_MOVES
 
-<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/MRk_YT">👤Mrk YT</a></b>
+<b>➥ ⭕ALL MOVIES CHANEL</b> : @MLM_MOVIE_1
 
-<b>➥ 🗣️Language</b> : <b>Python3<b>
-
-<b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
-
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2">💥Click Me</a></b>
+<b>➥ ⭕NEW MOVIES CHANEL</b> : @MLM_ROKES
 """
